9dce604c3efbf2a576fc6e516fcc225b007e0500
nicolanzi1 <nicolanzi@gmail.com>
2020-08-06 23:31:09 +0100

    Created tables for users, bands and albums and wrote the model, controller and views for the users, along with helper methods and scss stylesheets

---
+++ app/models/album.rb
@@ -0,0 +1,15 @@
+class Album < ApplicationRecord
+    belongs_to :band
+    has_many :tracks, dependent: :destroy
+
+    validates :band, :name, :year, presence: true
+    validates :live, inlcusion: { in: [true, false] }
+    validates :name, uniqueness: { scope: :band_id }
+    validates :year, numericality: { minimum: 1900, maximum: 9000 }
+
+    after_initialize :set_defaults
+
+    def set_defaults
+        self.live ||= false
+    end
+end
